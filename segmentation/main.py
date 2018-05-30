66cf152e4593b98b0af0a3862aed074bca6d5869
Hyuna915 <liuheguang.usc@gmail.com>
2018-05-30 00:20:50 -0700

    successfully loaded dataset

--- segmentation/main.py
+++ segmentation/main.py
@@ -2,9 +2,12 @@ import os
 import sys
 import numpy as np
 import tensorflow as tf
+
 slim = tf.contrib.slim
 import davis
 import matplotlib.pyplot as plt
+from model.dataset import load_data, _read_py_function
+
 # import model.segmentation_model.SegmentationModel
 
 
@@ -19,7 +22,9 @@ train_model = True
 # groundtruth_image_path = 'JPEGImages/480p/'
 
 tf.app.flags.DEFINE_string("sequence", "elephant", "which sequence")
-tf.app.flags.DEFINE_string("root_path", "/Users/hyuna915/Desktop/2018-CS231N/Final_Project/video_segmentation/davis-2017/data/DAVIS", "Available modes: train / show_examples / official_eval")
+tf.app.flags.DEFINE_string("root_path",
+                           "/Users/hyuna915/Desktop/2018-CS231N/Final_Project/video_segmentation/davis-2017/data/DAVIS",
+                           "Available modes: train / show_examples / official_eval")
 tf.app.flags.DEFINE_string("maskrcnn_label_path", "MaskRCNN/480p", "")
 tf.app.flags.DEFINE_string("osvos_label_path", "Results/Segmentations/480p/OSVOS", "")
 tf.app.flags.DEFINE_string("groundtruth_label_path", "Annotations/480p", "groundtruth_label_path")
@@ -31,7 +36,6 @@ tf.app.flags.DEFINE_integer("filter", 64, "weight")
 tf.app.flags.DEFINE_integer("kernel", 3, "weight")
 tf.app.flags.DEFINE_integer("pad", 1, "weight")
 tf.app.flags.DEFINE_integer("pool", 2, "weight")
-
 
 max_training_iters = 500
 FLAGS = tf.app.flags.FLAGS
@@ -51,15 +55,20 @@ def main(unused_argv):
   maskrcnn_image = maskrcnn_image[np.newaxis, ...]
   print "maskrcnn_image shape{}, unique {}".format(maskrcnn_image.shape, np.unique(maskrcnn_image))
 
-  groundtruth_label_image, _ = davis.io.imread_indexed(groundtruth_label_path+'00000.png')
+  groundtruth_label_image, _ = davis.io.imread_indexed(groundtruth_label_path + '00000.png')
   print "groundtruth label shape{}, unique {}".format(groundtruth_label_image.shape, np.unique(groundtruth_label_image))
 
   groundtruth_image_image = np.concatenate((osvos_image, maskrcnn_image), axis=0)
   print "groundtruth image shape{}, unique {}".format(groundtruth_image_image.shape, np.unique(groundtruth_image_image))
 
-
-
+  segmentation_dataset = load_data([osvos_label_path + '00000.png'], [maskrcnn_label_path + '00000.png'],
+                                   [groundtruth_label_path + '00000.png'])
+  iterator = segmentation_dataset.make_one_shot_iterator()
+  next_element = iterator.get_next()
+  # successfully load dataset
+  with tf.Session() as sess:
+    print(sess.run(next_element))
 
 
 if __name__ == "__main__":
-    tf.app.run()
+  tf.app.run()
--- segmentation/model/dataset.py
+++ segmentation/model/dataset.py
@@ -14,10 +14,12 @@ def _read_py_function(osvos_file, maskrcnn_file, groundtruth_file):
   maskrcnn_image = maskrcnn_image[np.newaxis, ...]
   # (2, 480, 854)
   input = np.concatenate((osvos_image, maskrcnn_image), axis=0)
+  print "###################{}".format(input.shape)
   return input, groundtruth_image
 
 def load_data(osvos_files, maskrcnn_files, groundtruth_files):
-  training_dataset = tf.data.Dataset.from_tensor_slices((osvos_files, maskrcnn_files, groundtruth_files))
+  file_tuple = (osvos_files, maskrcnn_files, groundtruth_files)
+  training_dataset = tf.data.Dataset.from_tensor_slices(file_tuple)
   training_dataset = training_dataset.map(
     lambda osvos_file, maskrcnn_file, groundtruth_file : tuple(
       tf.py_func(_read_py_function, [osvos_file, maskrcnn_file, groundtruth_file], [tf.uint8, tf.uint8])
