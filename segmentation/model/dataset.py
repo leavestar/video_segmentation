c91e46d9883eb73ca6bfa5af317877a54a1cd5e8
Hyuna915 <liuheguang.usc@gmail.com>
2018-06-01 22:58:16 -0700

    resize images

--- segmentation/model/dataset.py
+++ segmentation/model/dataset.py
@@ -16,18 +16,22 @@ def _read_py_function(osvos_file, maskrcnn_file, groundtruth_file):
   maskrcnn_image, _ = davis.io.imread_indexed(maskrcnn_file)
   groundtruth_image, _ = davis.io.imread_indexed(groundtruth_file)
 
+  if osvos_image.shape != (480, 854):
+    osvos_image = tf.image.resize_images(images=osvos_image, size=[480, 854])
+    logging.warn("Invalid dimension {} from osvos path {}".format(osvos_image.shape, osvos_file))
+
+  if maskrcnn_image.shape != (480, 854):
+    maskrcnn_image = tf.image.resize_images(images=maskrcnn_image, size=[480, 854])
+    logging.warn("Invalid dimension {} from osvos path {}".format(maskrcnn_image.shape, maskrcnn_file))
+
+  if groundtruth_image.shape != (480, 854):
+    groundtruth_image = tf.image.resize_images(images=groundtruth_image, size=[480, 854])
+    logging.warn("Invalid dimension {} from path {}".format(groundtruth_image.shape, groundtruth_file))
+
   osvos_image = osvos_image[..., np.newaxis]
   maskrcnn_image = maskrcnn_image[..., np.newaxis]
   groundtruth_image = groundtruth_image[..., np.newaxis]
 
-  if osvos_image.shape != (480, 854, 1):
-    raise Exception("Invalid dimension {} from osvos path {}".format(osvos_image.shape, osvos_file))
-
-  if maskrcnn_image.shape != (480, 854, 1):
-    raise Exception("Invalid dimension {} from osvos path {}".format(maskrcnn_image.shape, maskrcnn_file))
-
-  if groundtruth_image.shape != (480, 854, 1):
-    raise Exception("Invalid dimension {} from path {}".format(groundtruth_image.shape, groundtruth_file))
   # (480, 854, 2)
   input = np.concatenate((osvos_image.astype(np.float32), maskrcnn_image.astype(np.float32)), axis=2)
   groundtruth_image = groundtruth_image.astype(np.float32)
@@ -55,7 +59,7 @@ def load_data(osvos_files, maskrcnn_files, groundtruth_files):
 
 
 def dimension_validation(osvos_files, maskrcnn_files, groundtruth_files, logger):
-  all_image_valid = True
+  all_image_found = True
   invalid_seqs=set()
   for index in range(len(osvos_files)):
     try:
@@ -68,23 +72,21 @@ def dimension_validation(osvos_files, maskrcnn_files, groundtruth_files, logger)
       groundtruth_image = groundtruth_image[..., np.newaxis]
 
       if osvos_image.shape != (480, 854, 1):
-        logger.error("Invalid dimension {} from osvos path {}".format(osvos_image.shape, osvos_files[index]))
-        all_image_valid = False
+        logger.debug("Invalid dimension {} from osvos path {}".format(osvos_image.shape, osvos_files[index]))
 
       if maskrcnn_image.shape != (480, 854, 1):
-        logger.error("Invalid dimension {} from osvos path {}".format(maskrcnn_image.shape, maskrcnn_files[index]))
-        all_image_valid = False
+        logger.debug("Invalid dimension {} from osvos path {}".format(maskrcnn_image.shape, maskrcnn_files[index]))
         invalid_seqs.add(osvos_files[index].split('/')[-2])
 
       if groundtruth_image.shape != (480, 854, 1):
-        logger.error("Invalid dimension {} from path {}".format(groundtruth_image.shape, groundtruth_files[index]))
-        all_image_valid = False
+        logger.debug("Invalid dimension {} from path {}".format(groundtruth_image.shape, groundtruth_files[index]))
 
     except OSError as e:
       logger.error(e)
+      all_image_found = False
       continue
 
-  if all_image_valid is False:
+  if all_image_found is False:
     logger.error("Invalid sequence {}".format(invalid_seqs))
 
-  return all_image_valid
+  return all_image_found
