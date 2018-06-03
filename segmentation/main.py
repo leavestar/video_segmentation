074ef82f97f61ee8a8efd69ffd76e16b790af633
Hyuna915 <liuheguang.usc@gmail.com>
2018-06-03 12:18:36 -0700

    add logs

--- segmentation/main.py
+++ segmentation/main.py
@@ -17,7 +17,7 @@ import tensorflow.contrib.eager as tfe
 
 from davis import *
 
-env = "hyuna915"
+env = "cloud"
 path_config(env)
 
 tf.app.flags.DEFINE_boolean("train_mode", True, "enable training")
