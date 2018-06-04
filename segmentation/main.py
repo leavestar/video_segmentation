e5726a0557cb13a48767593ec86483b57ae7f994
Hyuna915 <liuheguang.usc@gmail.com>
2018-06-03 23:00:59 -0700

    remove log

--- segmentation/main.py
+++ segmentation/main.py
@@ -170,8 +170,7 @@ def main(unused_argv):
     with tf.control_dependencies(update_ops):
       train_op = optimizer.minimize(loss, global_step=global_step)
 
-  with tf.Session(config=tf.ConfigProto(allow_soft_placement=True,
-                                        log_device_placement=True)) as sess:
+  with tf.Session(config=tf.ConfigProto(allow_soft_placement=True)) as sess:
     sess.run(tf.global_variables_initializer())
 
     # For tensorboard
